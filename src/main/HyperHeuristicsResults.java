938dddb06ac531acb3023553e9bfc6049e418b57
jacksonpradolima <pradolima@live.com>
2015-06-09 20:34:29 -0300

    Mudança do endereçamento do arquivo de análise de resultados

--- src/main/HyperHeuristicsResults.java
+++ src/main/HyperHeuristicsResults.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package statistics;
+package main;
 
 import experiment.HyperHeuristicType;
 import java.io.FileNotFoundException;
